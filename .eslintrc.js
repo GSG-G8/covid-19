4a86ece3cf7f14e76495f05c4675a2673869d3bb
ahmadysalah <a.ahmad.salah@gmail.com>
2020-04-29 19:09:17 +0300

    fix eslint configuration  #3

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,45 +5,41 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 8,
     ecmaFeatures: {
-      impliedStrict: true //enable global strict mode (if ecmaVersion is 5 or greater)
-    }
+      impliedStrict: true, //enable global strict mode (if ecmaVersion is 5 or greater)
+    },
   },
   root: true,
   env: {
     browser: true,
     node: true,
-    jest: true
+    jest: true,
   },
   rules: {
-    "arrow-body-style": ["error", "as-needed"],
+    "react/prop-types": 0,
     "react/state-in-constructor": 0,
     //  allow .js extensions for JSX.
     "react/jsx-filename-extension": [
       1,
       {
-        extensions: [".js", ".jsx"]
-      }
+        extensions: [".js", ".jsx"],
+      },
     ],
     quotes: [
       2,
       "single",
       {
         avoidEscape: true, // allows strings to use single-quotes or double-quotes so long as the string contains a quote that would have to be escaped otherwise
-        allowTemplateLiterals: true // allows strings to use backticks
-      }
+        allowTemplateLiterals: true, // allows strings to use backticks
+      },
     ],
     // configure the prettier plugin
     "prettier/prettier": [
       "error",
       {
         trailingComma: "es5",
-        singleQuote: true
-      }
-    ]
+        singleQuote: true,
+      },
+    ],
   },
-  "settings": {
-    "react": {
-      "version": "detect"
-    },
-  plugins: ["react", "prettier"]
+  plugins: ["react", "prettier"],
 };
