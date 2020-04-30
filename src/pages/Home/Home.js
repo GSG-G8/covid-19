b1f305c786de3a5a6a87ff4b874638d323d6a083
ahmadysalah <a.ahmad.salah@gmail.com>
2020-04-30 03:05:26 +0300

    add axios npm and put all countries in state #7

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import "./style.css"
+import React, { useEffect, useState } from 'react';
+import './style.css';
+import axios from 'axios';
 
-function Home(){
+const URL =
+  'https://cors-anywhere.herokuapp.com/https://covid-api.mmediagroup.fr/v1/cases';
+
+function Home() {
+  const [country, setCountry] = useState();
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+    axios
+      .get(URL, {
+        cancelToken: source.token,
+      })
+      .then((result) => setCountry(Object.keys(result.data)))
+      .catch(() => 'there is no data');
+    return () => source.cancel();
+  }, []);
+
   return (
-  <div className="home">
-    <h1>Home </h1>
-  </div>
-  )
-};
+    <div className="home">
+      <h1> </h1>
+    </div>
+  );
+}
 
 export default Home;
